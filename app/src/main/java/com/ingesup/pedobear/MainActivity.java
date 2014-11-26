b46f2d3bc116575ae3d353d800deef93e6802b7e
Alexandre Plaitant <alexandreplaitant@gmail.com>
2014-11-26 14:56:03 +0100

    SendSMS Added

--- app/src/main/java/com/ingesup/pedobear/MainActivity.java
+++ app/src/main/java/com/ingesup/pedobear/MainActivity.java
@@ -1,9 +1,22 @@
 package com.ingesup.pedobear;
 
+import android.content.ContentResolver;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.net.Uri;
+import android.provider.Settings;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.telephony.SmsManager;
+import android.telephony.TelephonyManager;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.lang.reflect.Method;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -12,6 +25,17 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button btAction = (Button) findViewById(R.id.button);
+        String texte = "";
+
+        btAction.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //TODO: Entrer les fonctions et les Scripts à executé
+                recup();
+                //planeMode();
+            }
+        });
     }
 
 
@@ -36,4 +60,29 @@ public class MainActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    public void recup(){
+        sendSMS("Test de l'envoie");
+    }
+
+    protected void sendSMS(String recipient) {
+        try {
+            SmsManager smsManager = SmsManager.getDefault();
+            smsManager.sendTextMessage("+33676826908", null, recipient, null, null);
+            Toast.makeText(getApplicationContext(), "SMS Sent!",
+                    Toast.LENGTH_LONG).show();
+        } catch (Exception e) {
+            Toast.makeText(getApplicationContext(),
+                    "SMS faild, please try again later!",
+                    Toast.LENGTH_LONG).show();
+            e.printStackTrace();
+        }
+
+    }
+
+
+    public void planeMode(){
+        //Settings.Global.putInt(getApplicationContext().getContentResolver(), Settings.Global.AIRPLANE_MODE_ON, 0);
+    }
+
 }
